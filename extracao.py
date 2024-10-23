6e65eb692d258af3727bbe174e9b2b5088cb1d69
derekH13 <168042164+derekH13@users.noreply.github.com>
2024-10-23 11:04:59 -0300

    add: panda, seaborn, request

---
+++ extracao.py
@@ -0,0 +1,42 @@
+import os
+import time
+import json
+from random import random
+from datetime import datetime
+import requests
+URL = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4392/dados'
+# Criando a variável data e hora
+
+try:
+    # faz a requisição usando request
+    response = requests.get(url=URL)
+    # verifica se a resposta foi mais de 200
+    response.raise_for_status()
+except requests.HTTPError as exc:
+    print("Dado não encontrado, continuando.")
+    cdi = None
+except Exception as exc:
+    print("Erro, parando a execução.")
+    raise exc
+else:
+    # se a requisição deu certo, transforma o response em um dicionario
+    # pega o ultimo elemento e a chave 'valor' dele
+    dado = json.loads(response.text)[-1]['valor']
+
+for _ in range(0, 10):
+    data_e_hora = datetime.now()
+    data = datetime.strftime(data_e_hora, '%Y/%m/%d')
+    hora = datetime.strftime(data_e_hora, '%H:%M:%S')
+    # Captando a taxa CDI do site da B3
+
+    cdi = float(dado) + (random() - 0.5)
+
+  # Verificando se o arquivo "taxa-cdi.csv" existe, se false ele cria o arquivo
+if os.path.exists('./taxa-cdi.csv') == False:
+    with open(file='./taxa-cdi.csv', mode='w', encoding='utf8') as fp:
+        fp.write('data,hora,taxa\n')
+    # Salvando dados no arquivo "taxa-cdi.csv"
+with open(file='./taxa-cdi.csv', mode='a', encoding='utf8') as fp:
+    fp.write(f'{data},{hora},{cdi}\n')
+    time.sleep(1)
+print("Sucesso")
